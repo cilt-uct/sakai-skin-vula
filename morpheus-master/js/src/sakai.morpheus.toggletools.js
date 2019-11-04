2c1bad6b5eded80d28ae1a04d3202ebbf3ed15f0
Corne Oosthuizen <corne.oosthuizen@uct.ac.za>
2019-11-04 10:36:16 +0200

    VULA-3311: initial batch of transferring scss, bootstrap, and js to new branch.

--- morpheus-master/js/src/sakai.morpheus.toggletools.js
+++ morpheus-master/js/src/sakai.morpheus.toggletools.js
@@ -9,9 +9,10 @@ if (portal.toolsCollapsed === undefined) {
 }
 
 portal.updateToolsCollapsedPref = function (collapsed) {
-
-	var url = '/direct/userPrefs/updateKey/' + portal.user.id + '/sakai:portal:sitenav?toolsCollapsed=' + collapsed;
-	$PBJQ.ajax(url, {method: 'PUT', cache: false});
+	if (portal.user.id) {
+		var url = '/direct/userPrefs/updateKey/' + portal.user.id + '/sakai:portal:sitenav?toolsCollapsed=' + collapsed;
+		$PBJQ.ajax(url, {method: 'PUT', cache: false});
+	}
 };
 
 portal.toggleMinimizeNav = function () {
@@ -49,17 +50,19 @@ $PBJQ(document).ready(function () {
             var _top = ( -1 * ( $PBJQ('#toolMenu').height() - position.top ) );
             var subsitesPosition = ( MorpheusViewportHelper.isPhone() ) ? {
             	'display': 'block',
-            	'left': '-0.7rem',
             	'top': 0,
-            	'margin-top' : '0.3rem',
             	'overflow' : 'hidden'
             }:{
             	'display': 'block',
-            	'left': position.left + subsitesLink.width() + 6 + 'px',
+            	'left': $PBJQ('#toolMenu').width() + 7 + 'px',	// width of arrow border
             	'top': _top + 'px'
             }
             $PBJQ('#subSites').css(subsitesPosition);
             $PBJQ('#subSites').addClass('floating');
+			
+            // focus on first subsite for accessibility recommendations
+            $PBJQ('#subSites').find('li a').first().focus();
+			
             if ($PBJQ("#toggleSubsitesLink").position().top < 240) {
                 $PBJQ("#subSites.floating").addClass('ontop');
             }
