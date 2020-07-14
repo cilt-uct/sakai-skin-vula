572f1150a4734f9bd740cf0a8f008d63e08d5fd6
TurRil <lightscape2001@gmail.com>
2020-07-14 11:50:31 +0200

    VULA-3766: Build javascript

--- Gruntfile.js
+++ Gruntfile.js
@@ -147,7 +147,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   // grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
   // grunt.loadNpmTasks('grunt-ssh');
@@ -159,7 +159,7 @@ module.exports = function(grunt) {
     ,'sass'
     ,'postcss'
     //,'cssmin'
-    //,'uglify'
+    ,'uglify'
     ,'copy:dist'
   ]);
 
