13e40aa8c13dd83d7f15070905d94ddb2340334b
TurRil <lightscape2001@gmail.com>
2021-12-02 17:46:18 +0200

    Initial commit for 21.x

--- js/src/sakai.morpheus.update.presence.js
+++ js/src/sakai.morpheus.update.presence.js
@@ -1,20 +1,8 @@
-var attached = false; // initial load
-
-// Set the name of the hidden property and the change event for visibility
-var hidden, visibilityChange;
-if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support 
-  hidden = "hidden";
-  visibilityChange = "visibilitychange";
-} else if (typeof document.msHidden !== "undefined") {
-  hidden = "msHidden";
-  visibilityChange = "msvisibilitychange";
-} else if (typeof document.webkitHidden !== "undefined") {
-  hidden = "webkitHidden";
-  visibilityChange = "webkitvisibilitychange";
-}
+var activeTabListenerAttached = false,// initial load
+    handleVisibilityChange = function(){}; 
 
 function updatePresenceTimeout(_ms, _go) {
-    if (window.sakaiLastPresenceTimeOut != null) {
+    if (window.sakaiLastPresenceTimeOut !== null) {
         clearTimeout(window.sakaiLastPresenceTimeOut);
         window.sakaiLastPresenceTimeOut = null;
     }
@@ -23,78 +11,72 @@ function updatePresenceTimeout(_ms, _go) {
     }
 }
 
+function updatePresence() {
+  if (!activeTabListenerAttached) {
+    document.addEventListener("visibilitychange", handleVisibilityChange, false);
+    activeTabListenerAttached = true;
+  }
+
+  if (document.hidden) {
+    return;
+  }
+
+  $PBJQ.ajax({
+    url: sakaiPresenceFragment,
+    cache: false,
+    dataType : 'text',
+    success: function(frag){
+
+      var $presenceIframe = $PBJQ("#presenceIframe");
+      $presenceIframe.html(frag);
+
+      var $presenceCount = $PBJQ("#presenceCount");
+
+      if ($presenceIframe.is(':empty')) {
+        $presenceCount.html(' ');
+        $presenceCount.removeClass('present').addClass('empty');
+        location.reload();
+        return;
+      }
+
+      var userCount = $presenceIframe.find('.listUser').length;
+
+      // No need to attrct attention you are alone
+
+      if (userCount > 1) {
+        $presenceCount.html(userCount + '');
+        $presenceCount.removeClass('empty').addClass('present');
+      }
+
+      else if (userCount == 1) {
+        $PBJQ("#presenceCount").html(userCount + '');
+        $presenceCount.removeClass('present').addClass('empty');
+      }
+
+      else {
+        $PBJQ("#presenceCount").html(' ');
+        $presenceCount.removeClass('present').addClass('empty');
+      }
+
+      var chatUrl = $PBJQ('.nav-selected .icon-sakai-chat').attr('href');
+
+      $PBJQ('#presenceIframe .presenceList div.inChat span').wrap('<a href="' + chatUrl + '"></a>')
+      updatePresenceTimeout(30000, true); // 30 seconds
+    },
+
+    // If we get an error, wait 60 seconds before retry
+    error: function(request, strError){
+      updatePresenceTimeout(60000, true);
+    }
+  });
+}
+
 // Triggers when visibility changes
-function handleVisibilityChange() {
+handleVisibilityChange = function(event) {
     if (document.hidden) {
-        //console.log("hidden");
         updatePresenceTimeout(window.sakaiPresenceTimeDelay, false); //stop
     } else  {
-        //console.log("visible");
         updatePresenceTimeout(window.sakaiPresenceTimeDelay, false); 
         updatePresence(); //start immediately
     }
-}
-
-function updatePresence() {
-    if (!attached) {
-        document.addEventListener("visibilitychange", handleVisibilityChange, false);
-        attached = true;
-        // console.log("initPresence: " + attached);
-    }
-
-    if (document.hidden) {
-        // console.log("we started hidden - so wait");
-        return;
-    }
-
-    //console.log("sakaiPresenceTimeDelay: "+ window.sakaiPresenceTimeDelay);
-    //console.log("sakaiPresenceFragment: "+ window.sakaiPresenceFragment);
-    //console.log("sakaiLastPresenceTimeOut: "+ window.sakaiLastPresenceTimeOut);
-    //console.log(new Date().toLocaleTimeString());
-
-    $PBJQ.ajax({
-        url: sakaiPresenceFragment,
-        cache: false,
-        dataType : 'text',
-        success: function(frag){
-
-            var $presenceIframe = $PBJQ("#presenceIframe");
-            $presenceIframe.html(frag);
-
-            var $presenceCount = $PBJQ("#presenceCount");
-
-            if ($presenceIframe.is(':empty')) {
-                $presenceCount.html(' ');
-                $presenceCount.removeClass('present').addClass('empty');
-                location.reload();
-                return;
-            }
-
-            var userCount = $presenceIframe.find('.listUser').length;
-
-            // No need to attract attention you are alone :(
-            if (userCount > 1) {
-                $presenceCount.html(userCount + '');
-                $presenceCount.removeClass('empty').addClass('present');
-            }
-            else if (userCount == 1) {
-                $PBJQ("#presenceCount").html(userCount + '');
-                $presenceCount.removeClass('present').addClass('empty');
-            }
-            else {
-                $PBJQ("#presenceCount").html(' ');
-                $presenceCount.removeClass('present').addClass('empty');
-            }
-
-            var chatUrl = $PBJQ('.nav-selected .icon-sakai-chat').attr('href');
-            $PBJQ('#presenceIframe .presenceList div.inChat span').wrap('<a href="' + chatUrl + '"></a>');
-            updatePresenceTimeout(60000, true); // 60 seconds
-            // console.log(" > "+ new Date().toLocaleTimeString());
-        },
-        error: function(request, strError){
-            // If we get an error, wait 120 seconds before retry
-            updatePresenceTimeout(120000, true);
-            // console.log(" > "+ new Date().toLocaleTimeString());
-        }
-    });
-}
+};
--- morpheus-master/compile-skin.md
+++ morpheus-master/compile-skin.md
@@ -5,15 +5,19 @@ Then type:
 
 `mvn clean install`
 
-If you want to change any configuration in _defaults.scss to easily customize your own skin you just need to point to the file with those changes. It will be copied by maven in the proper place:
+If you want to keep the out of the box look of Sakai and theme it to your institution or organization's brand, uncomment the appropriate lines in `library/src/morpheus-master/sass/themes/_custom.scss` to set the CSS Custom Property values. Then type:
+
+`mvn clean install`
+
+If you want to further change the configuration in _defaults.scss to customize your skin, you need to point to the file with those changes. It will be copied by Maven into the proper place:
 
 `mvn clean install -Dsakai.skin.customization.file=/folder/to/your/file.scss`
 
 For example:
 
-- To generate without icons:
+ - To generate without icons: 
  `mvn clean install -Dsakai.skin.customization.file=./src/morpheus-master/sass/examples/_customization_example_withouticons.scss`
-- To generate with another google font:
+ - To generate with another google font: 
  `mvn clean install -Dsakai.skin.customization.file=./src/morpheus-master/sass/examples/_customization_example_typography.scss`
 
 By default source is morpheus-master and target is morpheus-default, but you can change these folders typing:
@@ -24,10 +28,4 @@ We have uploaded a non-icons version compiled by:
 
 `mvn clean install -Dsakai.skin.target=morpheus-default-noicons -Dsakai.skin.customization.file=./src/morpheus-master/sass/examples/_customization_example_withouticons.scss`
 
-Feel free to repeat this commands to generate as many skins as you want.
-
-## Common issues
-
-**I have edited `/src/morpheus-master/sass/_defaults.scss` or `_customization.scss` and don't see my edits reflected after a compile and deploy.**
-
-Check the `/src/morpheus-master/sass/base_material-defaults.scss` file to see if it is overriding your changes. That file was introduced in version 20 and refactored in 21, and because it is missing `!default` statements, it often overrides the variables set in other files. We recommend changing both `_defaults.scss` and `_material-defaults.scss` at this time.
+Feel free to repeat these commands to generate as many skins as you want.
